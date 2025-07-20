78849c04ce91bffc2de4c7ce01171e7d100867dc
Karmpatel18 <karmpatel1203@gmail.com>
2025-07-20 22:41:54 +0530

    embedder add

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,33 @@
 from fastapi import FastAPI
 import dotenv
-dotenv.load_dotenv()
+import os
+from embedder import get_embedding
+from pinecone import Pinecone, ServerlessSpec # type: ignore
 
+
+# Load environment variables
+dotenv.load_dotenv()
+api = os.getenv("API_KEY")
+
+# Initialize FastAPI app
 app = FastAPI()
-api = dotenv.get_key(dotenv.find_dotenv(), "API_KEY")
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello World", "api_key": api}
+    return {"message": "Hello World"}
+
+pc = Pinecone(api_key=api)
+
+# Create a dense index with integrated embedding
+index_name = "quickstart-py"
+if not pc.has_index(index_name):
+    pc.create_index_for_model(
+        name=index_name,
+        cloud="aws",
+        region="us-east-1",
+        embed={
+            "model":"llama-text-embed-v2",
+            "field_map":{"text": "chunk_text"}
+        }
+    )
+    
