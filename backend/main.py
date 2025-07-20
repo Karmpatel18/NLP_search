44f1ab2804bb17be03d932ade92cf6165bd65067
Karmpatel18 <karmpatel1203@gmail.com>
2025-07-20 21:33:09 +0530

    env var api set

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+import dotenv
+dotenv.load_dotenv()
 
 app = FastAPI()
-
+api = dotenv.get_key(dotenv.find_dotenv(), "API_KEY")
 @app.get("/")
 def read_root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World", "api_key": api}
